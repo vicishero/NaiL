ce940a00754d0c077e7ecfc38aeae43c36790be7
v <12@qq.com>
2026-06-10 15:06:42 +0800

    feat(contract): 修改KOL合约支持有效期和不可转移，Minter支持按月购买和续费

--- contract/contracts/NailoKOL.sol
+++ contract/contracts/NailoKOL.sol
@@ -7,20 +7,52 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract NailoKOL is ERC721, ERC721Burnable, Ownable {
     uint256 private _nextTokenId;
+    address public minter;
+    mapping(address => uint256) public expireTime;
+
+    modifier onlyMinter() {
+        require(msg.sender == minter, "NailoKOL: caller is not the minter");
+        _;
+    }
+
+    constructor() ERC721("NailoKOL", "KOL") Ownable(msg.sender) {}
+
+    function setMinter(address _minter) public onlyOwner {
+        minter = _minter;
+    }
+
+    function mint(address to) public onlyMinter {
+        require(balanceOf(to) == 0, "NailoKOL: already has a token");
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+    }
+
+    function setExpireTime(address user, uint256 time) public onlyMinter {
+        expireTime[user] = time;
+    }
 
     function nextTokenId() public view returns (uint256) {
         return _nextTokenId;
     }
 
-    constructor() ERC721("NailoKOL", "KOL") Ownable(msg.sender) {}
-
-    function safeMint(address to) public onlyOwner {
-        uint256 tokenId = _nextTokenId++;
-        _safeMint(to, tokenId);
-    }
-
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
         _requireOwned(tokenId);
         return string(abi.encodePacked("https://api.nailo.io/kol/", Strings.toString(tokenId), ".json"));
+    }
+
+    function _update(address to, uint256 tokenId, address auth) internal override returns (address) {
+        address from = _ownerOf(tokenId);
+        if (from != address(0) && to != address(0)) {
+            revert("NailoKOL: transfer is disabled");
+        }
+        return super._update(to, tokenId, auth);
+    }
+
+    function approve(address to, uint256 tokenId) public pure override {
+        revert("NailoKOL: approve is disabled");
+    }
+
+    function setApprovalForAll(address operator, bool approved) public pure override {
+        revert("NailoKOL: setApprovalForAll is disabled");
     }
 }
--- contract/contracts/NailoKolMinter.sol
+++ contract/contracts/NailoKolMinter.sol
@@ -8,42 +8,51 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 contract NailoKolMinter is AccessControl {
     using SafeERC20 for IERC20;
 
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
     NailoKOL public immutable kolToken;
     IERC20 public usdt;
-    uint256 public price = 200 * 10**6;
+    address public treasury;
+    uint256 public monthlyPrice = 10 * 10**6;
 
-    event Minted(address indexed to, uint256 indexed tokenId);
-    event PriceUpdated(uint256 oldPrice, uint256 newPrice);
+    event Minted(address indexed user, uint256 indexed tokenId, uint256 months);
+    event Renewed(address indexed user, uint256 months);
+    event MonthlyPriceUpdated(uint256 oldPrice, uint256 newPrice);
     event UsdtAddressUpdated(address oldUsdt, address newUsdt);
+    event TreasuryUpdated(address oldTreasury, address newTreasury);
 
-    constructor(address _kolToken) {
+    constructor(address _kolToken, address _treasury) {
         require(_kolToken != address(0), "Minter: invalid token address");
+        require(_treasury != address(0), "Minter: invalid treasury address");
         kolToken = NailoKOL(_kolToken);
+        treasury = _treasury;
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(MINTER_ROLE, msg.sender);
         _grantRole(ADMIN_ROLE, msg.sender);
     }
 
-    function mint(address to) public onlyRole(MINTER_ROLE) {
-        uint256 tokenId = kolToken.nextTokenId();
-        kolToken.safeMint(to);
-        emit Minted(to, tokenId);
-    }
-
-    function buy() public {
+    function buy(uint256 months) public {
+        require(months > 0, "Minter: months must be greater than 0");
         require(address(usdt) != address(0), "Minter: USDT not set");
-        usdt.safeTransferFrom(msg.sender, address(this), price);
-        uint256 tokenId = kolToken.nextTokenId();
-        kolToken.safeMint(msg.sender);
-        emit Minted(msg.sender, tokenId);
+
+        uint256 amount = monthlyPrice * months;
+        usdt.safeTransferFrom(msg.sender, treasury, amount);
+
+        if (kolToken.balanceOf(msg.sender) == 0) {
+            uint256 tokenId = kolToken.nextTokenId();
+            kolToken.mint(msg.sender);
+            kolToken.setExpireTime(msg.sender, block.timestamp + months * 30 days);
+            emit Minted(msg.sender, tokenId, months);
+        } else {
+            uint256 currentExpire = kolToken.expireTime(msg.sender);
+            uint256 newExpire = currentExpire > block.timestamp ? currentExpire : block.timestamp;
+            kolToken.setExpireTime(msg.sender, newExpire + months * 30 days);
+            emit Renewed(msg.sender, months);
+        }
     }
 
-    function setPrice(uint256 _price) public onlyRole(ADMIN_ROLE) {
-        emit PriceUpdated(price, _price);
-        price = _price;
+    function setMonthlyPrice(uint256 _monthlyPrice) public onlyRole(ADMIN_ROLE) {
+        emit MonthlyPriceUpdated(monthlyPrice, _monthlyPrice);
+        monthlyPrice = _monthlyPrice;
     }
 
     function setUsdtAddress(address _usdt) public onlyRole(ADMIN_ROLE) {
@@ -51,15 +60,8 @@ contract NailoKolMinter is AccessControl {
         usdt = IERC20(_usdt);
     }
 
-    function withdrawUsdt(address to, uint256 amount) public onlyRole(ADMIN_ROLE) {
-        usdt.safeTransfer(to, amount);
-    }
-
-    function addMinter(address account) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        grantRole(MINTER_ROLE, account);
-    }
-
-    function removeMinter(address account) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        revokeRole(MINTER_ROLE, account);
+    function setTreasury(address _treasury) public onlyRole(ADMIN_ROLE) {
+        emit TreasuryUpdated(treasury, _treasury);
+        treasury = _treasury;
     }
 }
