5e5de5ac172f6a8aaec50db92ecad40596b9211e
v <12@qq.com>
2026-06-10 14:57:31 +0800

    feat(contract): 修改NailoKolMinter，移除batchMint，增加USDT购买功能

--- contract/contracts/NailoKolMinter.sol
+++ contract/contracts/NailoKolMinter.sol
@@ -3,20 +3,28 @@ pragma solidity ^0.8.34;
 
 import "./NailoKOL.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract NailoKolMinter is AccessControl {
+    using SafeERC20 for IERC20;
+
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
     NailoKOL public immutable kolToken;
+    IERC20 public usdt;
+    uint256 public price = 200 * 10**6;
 
     event Minted(address indexed to, uint256 indexed tokenId);
-    event BatchMinted(address indexed to, uint256[] tokenIds);
+    event PriceUpdated(uint256 oldPrice, uint256 newPrice);
+    event UsdtAddressUpdated(address oldUsdt, address newUsdt);
 
     constructor(address _kolToken) {
         require(_kolToken != address(0), "Minter: invalid token address");
         kolToken = NailoKOL(_kolToken);
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
+        _grantRole(ADMIN_ROLE, msg.sender);
     }
 
     function mint(address to) public onlyRole(MINTER_ROLE) {
@@ -25,16 +33,26 @@ contract NailoKolMinter is AccessControl {
         emit Minted(to, tokenId);
     }
 
-    function batchMint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
-        require(amount > 0, "Minter: amount must be greater than 0");
-        require(amount <= 100, "Minter: amount exceeds limit");
+    function buy() public {
+        require(address(usdt) != address(0), "Minter: USDT not set");
+        usdt.safeTransferFrom(msg.sender, address(this), price);
+        uint256 tokenId = kolToken.nextTokenId();
+        kolToken.safeMint(msg.sender);
+        emit Minted(msg.sender, tokenId);
+    }
 
-        uint256[] memory tokenIds = new uint256[](amount);
-        for (uint256 i = 0; i < amount; i++) {
-            tokenIds[i] = kolToken.nextTokenId();
-            kolToken.safeMint(to);
-        }
-        emit BatchMinted(to, tokenIds);
+    function setPrice(uint256 _price) public onlyRole(ADMIN_ROLE) {
+        emit PriceUpdated(price, _price);
+        price = _price;
+    }
+
+    function setUsdtAddress(address _usdt) public onlyRole(ADMIN_ROLE) {
+        emit UsdtAddressUpdated(address(usdt), _usdt);
+        usdt = IERC20(_usdt);
+    }
+
+    function withdrawUsdt(address to, uint256 amount) public onlyRole(ADMIN_ROLE) {
+        usdt.safeTransfer(to, amount);
     }
 
     function addMinter(address account) public onlyRole(DEFAULT_ADMIN_ROLE) {
